09bf8d0e28090d250ece7b610ad45f7043d86eb3
Bassam-Z <bassamz.ali@hotmail.com>
2023-08-27 14:00:41 +0200

    Das Projekt ist komplett

---
+++ src/App.vue
@@ -0,0 +1,81 @@
+
+<template>
+    <div class="wrapper">
+      <h1>Reaction Timer</h1>
+      <button @click="start" :disabled="isPlaying">Play</button>
+      <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+      <Results v-if="showResults" :score="score" />
+    </div>
+</template>
+
+  <script>
+  import Block from './components/Block.vue';
+  import Results from './components/Results.vue';
+  export default{
+    components: {Block, Results},
+    data(){
+      return{
+        isPlaying: false,
+        delay: null,
+        score: null,
+        showResults: false
+      }
+    },
+    methods: {
+      start(){
+        this.delay = 2000 + Math.random() * 5000;
+        this.isPlaying = true;
+        this.showResults = false;
+      },
+      endGame(reactionTime){
+        this.score = reactionTime;
+        this.isPlaying  = false;
+        this.showResults = true;
+      }
+    }
+  }
+</script>
+<style scoped>
+header {
+  line-height: 1.5;
+}
+button {
+  background: green;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+
+button[disabled]{
+  opacity: 0.2;
+  cursor: not-allowed;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/Results.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <p >Reaction Time: {{ score }} ms</p>
+        <p class="rank">{{ rank }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props:[ 'score' ],
+    data(){
+        return{
+            rank: null,
+        }
+    },
+    mounted(){
+        if (this.score < 250){
+            this.rank = 'Fingers';
+        }else if (this.score < 400){
+            this.rank = 'Reflexes';
+        }else {
+            this.rank = ' pace...'
+        }
+    }
+
+}
+</script>
+
+<style>
+.rank{
+    font-size: 1.4em;
+    color: aquamarine;
+    font-weight: bold;
+}
+</style>
